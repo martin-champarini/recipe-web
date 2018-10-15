88a0f557eef39d53efd5a8b351fcae54df040ede
Martin Champarini <martin.champarini.dev@gmail.com>
2018-10-15 00:18:11 -0300

    Init Commit

---
+++ src/main/java/com/recipe/web/recipeweb/RecipeWebApplication.java
@@ -0,0 +1,12 @@
+package com.recipe.web.recipeweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeWebApplication.class, args);
+    }
+}
