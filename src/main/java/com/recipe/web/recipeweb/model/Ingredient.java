c12d7e216060d7562796379a452c7c93a1a82b0d
Martin Champarini <martin.champarini.dev@gmail.com>
2018-10-17 23:19:02 -0300

    Added OneToMany and ManyToOne

---
+++ src/main/java/com/recipe/web/recipeweb/model/Ingredient.java
@@ -0,0 +1,43 @@
+package com.recipe.web.recipeweb.model;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+public class Ingredient {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String description;
+    private BigDecimal amount;
+
+    @ManyToOne
+    private Recipe recipe;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+}
---
+++ src/main/java/com/recipe/web/recipeweb/model/Recipe.java
@@ -0,0 +1,94 @@
+package com.recipe.web.recipeweb.model;
+
+import org.hibernate.annotations.Cascade;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class Recipe {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String description;
+    private Integer prepTime;
+    private Integer cookTime;
+    private Integer serving;
+    private String source;
+    private String url;
+    private String directions;
+
+    @Lob
+    private Byte[] image;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Notes notes;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredient;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getPrepTime() {
+        return prepTime;
+    }
+
+    public void setPrepTime(Integer prepTime) {
+        this.prepTime = prepTime;
+    }
+
+    public Integer getCookTime() {
+        return cookTime;
+    }
+
+    public void setCookTime(Integer cookTime) {
+        this.cookTime = cookTime;
+    }
+
+    public Integer getServing() {
+        return serving;
+    }
+
+    public void setServing(Integer serving) {
+        this.serving = serving;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getDirections() {
+        return directions;
+    }
+
+    public void setDirections(String directions) {
+        this.directions = directions;
+    }
+}
